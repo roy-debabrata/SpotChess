d290af8f6fbd9ff3b17d9c830bba3d657e4a3d59
Debabrata Roy <debabrataroy.318@gmail.com>
2023-08-12 20:24:19 +0530

    Adding more perft tests.

--- src/test/java/com/debabrata/spotchess/support/PerftTest.java
+++ src/test/java/com/debabrata/spotchess/support/PerftTest.java
@@ -48,6 +48,35 @@ public class PerftTest {
         perft("r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10", 6,6923051137L);
     }
 
+    @Test
+    public void position7() {
+        perft("1k6/1b6/8/8/7R/8/8/4K2R b K - 0 1", 5, 1063513);
+    }
+
+    @Test
+    public void moreEnPassantIssues() {
+        // TalkChess PERFT Tests (by Martin Sedlak)
+        perft("3k4/3p4/8/K1P4r/8/8/8/8 b - - 0 1", 6, 1134888);
+        perft("8/8/4k3/8/2p5/8/B2P2K1/8 w - - 0 1", 6, 1015133);
+        perft("8/8/1k6/2b5/2pP4/8/5K2/8 b - d3 0 1", 6, 1440467);
+    }
+
+    @Test
+    public void moreChecksAndStalemates() {
+        // TalkChess PERFT Tests (by Martin Sedlak)
+        perft("5k2/8/8/8/8/8/8/4K2R w K - 0 1", 6, 661072);
+        perft("3k4/8/8/8/8/8/8/R3K3 w Q - 0 1", 6, 803711);
+        perft("r3k2r/1b4bq/8/8/8/8/7B/R3K2R w KQkq - 0 1", 4, 1274206);
+        perft("r3k2r/8/3Q4/8/8/5q2/8/R3K2R b KQkq - 0 1", 4, 1720476);
+        perft("2K2r2/4P3/8/8/8/8/8/3k4 w - - 0 1", 6, 3821001);
+        perft("8/8/1P2K3/8/2n5/1q6/8/5k2 b - - 0 1", 5, 1004658);
+        perft("4k3/1P6/8/8/8/8/K7/8 w - - 0 1", 6, 217342);
+        perft("8/P1k5/K7/8/8/8/8/8 w - - 0 1", 6, 92683);
+        perft("K1k5/8/P7/8/8/8/8/8 w - - 0 1", 6, 2217);
+        perft("8/k1P5/8/1K6/8/8/8/8 w - - 0 1", 7, 567584);
+        perft("8/8/2k5/5q2/5n2/8/5K2/8 b - - 0 1", 4, 23527);
+    }
+
     private void perft(String fen, int depth, long expectedResult) {
         Game game = fenParser.getGame(fen);
 
